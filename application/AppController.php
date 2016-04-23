23f045dc06d58cb4a5b07292db27d6cfdaa2681c
Rémi Fumeron <patatozaur@gmail.com>
2016-04-23 20:10:32 +0200

    Créations de controllers pour l'admin

---
+++ application/AppController.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Rémi
+ * Date: 23/04/2016
+ * Time: 18:48
+ */
+class AppController extends CoreController
+{
+
+}
---
+++ application/controller/AdminController.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Rémi
+ * Date: 23/04/2016
+ * Time: 18:49
+ */
+class AdminController extends AppController
+{
+
+}
